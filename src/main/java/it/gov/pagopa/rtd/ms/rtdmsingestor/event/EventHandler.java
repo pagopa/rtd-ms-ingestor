fb3ac4d809c090bc07ac8e0c3aaaae257f8cbe22
Giovanni <g.mancini.0@gmail.com>
2022-03-16 15:30:11 +0100

    [CEN-1446] Configure basic event handler

---
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmsingestor/event/EventHandler.java
@@ -0,0 +1,38 @@
+package it.gov.pagopa.rtd.ms.rtdmsingestor.event;
+
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+import it.gov.pagopa.rtd.ms.rtdmsingestor.model.EventGridEvent;
+
+/**
+ * Component defining the processing steps in response to storage events.
+ */
+@Configuration
+@Getter
+@Slf4j
+public class EventHandler {
+
+  /**
+   * Constructor.
+   *
+   * @param decrypterImpl an instance of a Decrypter
+   * @param blobRestConnectorImpl an instance of a blobRestConnector
+   * @return a consumer for Event Grid events
+   */
+  @Bean
+  public Consumer<Message<List<EventGridEvent>>> blobStorageConsumer() {
+    return message -> message.getPayload().stream().
+        map(m -> {
+          log.error("{}", m.getSubject());
+          return m;
+        })
+        .collect(Collectors.toList());
+  }
+
+}
