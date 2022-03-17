d1aec39115a214a33df4b4f97cb3dfe548e2a171
Giovanni <g.mancini.0@gmail.com>
2022-03-17 09:34:04 +0100

    [CEN-1447] Add basic producer test

--- src/test/java/it/gov/pagopa/rtd/ms/rtdmsingestor/RtdMsIngestorApplicationTests.java
+++ src/test/java/it/gov/pagopa/rtd/ms/rtdmsingestor/RtdMsIngestorApplicationTests.java
@@ -2,6 +2,7 @@ package it.gov.pagopa.rtd.ms.rtdmsingestor;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Supplier;
 import static org.awaitility.Awaitility.await;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -13,9 +14,11 @@ import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.integration.support.MessageBuilder;
 import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.messaging.Message;
 import org.springframework.test.context.ActiveProfiles;
 import it.gov.pagopa.rtd.ms.rtdmsingestor.event.EventHandler;
 import it.gov.pagopa.rtd.ms.rtdmsingestor.model.EventGridEvent;
+import it.gov.pagopa.rtd.ms.rtdmsingestor.model.Transaction;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -27,7 +30,10 @@ class RtdMsIngestorApplicationTests {
 	EventHandler eh;
 
 	@Autowired
-	private StreamBridge channel;
+	private StreamBridge stream;
+
+	@Autowired
+	private Supplier<Message<Transaction>> producer;
 
 	@Test
 	void shouldConsumeMessage() {
@@ -45,9 +51,20 @@ class RtdMsIngestorApplicationTests {
 
 		await().atMost(Duration.ofSeconds(10)).untilAsserted(() -> {
 
-			channel.send("blobStorageConsumer-in-0", MessageBuilder.withPayload(my_list).build());
+			stream.send("blobStorageConsumer-in-0", MessageBuilder.withPayload(my_list).build());
 
 			verify(eh, times(1)).blobStorageConsumer();
+
+		});
+
+	}
+
+	@Test
+	void shouldProduceMessage() {
+
+		await().atMost(Duration.ofSeconds(10)).untilAsserted(() -> {
+			stream.send("rtdTrxProducer-out-0", producer.get());
+			verify(eh, times(1)).rtdTrxProducer();
 
 		});
 
