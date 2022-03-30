dfe9d5d3bd8d7ab8d4e1fd1484754476cfdf5ad3
TommasoLencioni <tommaso.lencioni@pagopa.it>
2022-03-30 16:53:25 +0200

    [CEN-1449] Refactor trx consumer event handler.

--- src/test/java/it/gov/pagopa/rtd/ms/rtdmsingestor/event/EventHandlerIntegration.java
+++ src/test/java/it/gov/pagopa/rtd/ms/rtdmsingestor/event/EventHandlerIntegration.java
@@ -1,5 +1,7 @@
 package it.gov.pagopa.rtd.ms.rtdmsingestor.event;
 
+import it.gov.pagopa.rtd.ms.rtdmsingestor.model.Transaction;
+import it.gov.pagopa.rtd.ms.rtdmsingestor.service.BlobRestConnector;
 import java.util.function.Consumer;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
@@ -7,9 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.Message;
-import it.gov.pagopa.rtd.ms.rtdmsingestor.model.BlobApplicationAware;
-import it.gov.pagopa.rtd.ms.rtdmsingestor.model.Transaction;
-import it.gov.pagopa.rtd.ms.rtdmsingestor.service.BlobRestConnector;
 
 /**
  * Component defining the processing steps in response to storage events.
@@ -22,9 +21,13 @@ public class EventHandlerIntegration {
   @Autowired
   BlobRestConnector blobRestConnector;
 
+  /**
+   * Event handler triggered by the insertion of a transaction on the input queue.
+   *
+   * @return a consumer handling events.
+   */
   @Bean
-  public Consumer<Message<Transaction>> rtdTrxConsumer(
-      BlobApplicationAware blobApplicationAware) {
+  public Consumer<Message<Transaction>> rtdTrxConsumer() {
     return message -> {
       blobRestConnector.test(message);
       log.info("\n\n\n\n\n\nTEST\n\n\n\n\n\n");
