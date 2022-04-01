0e1b36dad9d97c39b08648924c358ff8b7f3a197
TommasoLencioni <tommaso.lencioni@pagopa.it>
2022-04-01 18:04:57 +0200

    [CEN-1530] add and refactor of transaction fields. Association with positions inside CSV.

--- src/main/java/it/gov/pagopa/rtd/ms/rtdmsingestor/model/Transaction.java
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmsingestor/model/Transaction.java
@@ -7,45 +7,77 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import net.sf.jsefa.csv.annotation.CsvDataType;
+import net.sf.jsefa.csv.annotation.CsvField;
 
 /**
  * This class represents the object containing transaction fields as attributes.
+ * The format is based on the one specified at:
+ * https://docs.pagopa.it/digital-transaction-register/v/digital-transaction-filter/acquirer-integration-with-pagopa-centrostella/integration/standard-pagopa-file-transactions
  */
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(of = {"idTrxAcquirer", "acquirerCode", "trxDate"}, callSuper = false)
+@CsvDataType()
 public class Transaction {
 
-  String idTrxAcquirer;
-
+  @CsvField(pos = 1)
   String acquirerCode;
 
-  OffsetDateTime trxDate;
-
-  String hpan;
-
+  @CsvField(pos = 2)
   String operationType;
 
+  @CsvField(pos = 3)
   String circuitType;
 
+  @CsvField(pos = 4)
+  String hpan;
+
+  //TODO sistema il formato
+  @CsvField(pos = 5)
+  String trxDate;
+
+  @CsvField(pos = 6)
+  String idTrxAcquirer;
+
+  @CsvField(pos = 7)
   String idTrxIssuer;
 
+  @CsvField(pos = 8)
   String correlationId;
 
-  BigDecimal amount;
+  @CsvField(pos = 9)
+  BigDecimal totalAmount;
 
+  @CsvField(pos = 10)
   String amountCurrency;
 
-  String mcc;
-
+  @CsvField(pos = 11)
   String acquirerId;
 
+  @CsvField(pos = 12)
   String merchantId;
 
+  @CsvField(pos = 13)
   String terminalId;
 
+  @CsvField(pos = 14)
   String bin;
 
+  @CsvField(pos = 15)
+  String mcc;
+
+  @CsvField(pos = 16)
+  String fiscalCode;
+
+  @CsvField(pos = 17)
+  String vat;
+
+  @CsvField(pos = 18)
+  String posType;
+
+  @CsvField(pos = 19)
+  String par;
 }
