6ff1a7ed5b4478919f29e9045e3fb8dcb9a6ea12
TommasoLencioni <tommaso.lencioni@pagopa.it>
2022-04-12 10:52:06 +0200

    [CEN-1560] refactor blob application aware test.

--- src/test/java/it/gov/pagopa/rtd/ms/rtdmsingestor/model/BlobApplicationAwareTest.java
+++ src/test/java/it/gov/pagopa/rtd/ms/rtdmsingestor/model/BlobApplicationAwareTest.java
@@ -2,10 +2,21 @@ package it.gov.pagopa.rtd.ms.rtdmsingestor.model;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertSame;
 
 import it.gov.pagopa.rtd.ms.rtdmsingestor.model.BlobApplicationAware.Status;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import org.apache.commons.io.FileUtils;
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -23,37 +34,63 @@ class BlobApplicationAwareTest {
   @Value("${ingestor.resources.base.path}")
   String resources;
 
+  @Value("${ingestor.resources.base.path}/tmp")
+  String tmpDirectory;
+
   @MockBean
   CloseableHttpClient closeableHttpClient;
 
+  String containerRtd = "rtd-transactions-32489876908u74bh781e2db57k098c5ad034341i8u7y";
+  String blobNameRtd = "CSTAR.99910.TRNLOG.20220316.164707.001.csv.pgp.decrypted";
+
+  BlobApplicationAware fakeBlob;
+
+  @BeforeEach
+  void setUp() throws IOException {
+    //Create dummy files to be deleted
+    File blobFile = Path.of(tmpDirectory, blobNameRtd).toFile();
+    blobFile.getParentFile().mkdirs();
+    blobFile.createNewFile();
+
+    FileOutputStream blobStream = new FileOutputStream(
+        Path.of(tmpDirectory, blobNameRtd).toString());
+
+    //Instantiate a fake blob with empty content
+    fakeBlob = new BlobApplicationAware(
+        "/blobServices/default/containers/" + containerRtd + "/blobs/" + blobNameRtd);
+    fakeBlob.setTargetDir(tmpDirectory);
+  }
+
+  @AfterEach
+  void cleanTmpFiles() throws IOException {
+    FileUtils.deleteDirectory(Path.of(tmpDirectory).toFile());
+  }
+
+
   @Test
   void shouldMatchRegexRtd() {
-    String container = "rtd-transactions-32489876908u74bh781e2db57k098c5ad034341i8u7y";
-    String blob = "CSTAR.99910.TRNLOG.20220228.103107.001.csv.pgp";
-    String blobUri = "/blobServices/default/containers/" + container + "/blobs/" + blob;
-    BlobApplicationAware myBlob = new BlobApplicationAware(blobUri);
-    assertSame(Status.RECEIVED, myBlob.getStatus());
+    assertSame(Status.RECEIVED, fakeBlob.getStatus());
   }
 
   @Test
   void shouldNotMatchRegexEventNotOfInterest(CapturedOutput output) {
-    String container = "ade-transactions-32489876908u74bh781e2db57k098c5ad034341i8u7y";
-    String blob = "ADE.99910.TRNLOG.20220228.203107.001.csv.pgp";
-    String blobUri = "/blobServices/default/containers/" + container + "/blobs/" + blob;
+    String containerAde = "ade-transactions-32489876908u74bh781e2db57k098c5ad034341i8u7y";
+    String blobAde = "ADE.99910.TRNLOG.20220228.203107.001.csv.pgp";
+    String blobUri = "/blobServices/default/containers/" + containerAde + "/blobs/" + blobAde;
     BlobApplicationAware myBlob = new BlobApplicationAware(blobUri);
     assertSame(Status.INIT, myBlob.getStatus());
     assertThat(output.getOut(), containsString("Event not of interest:"));
   }
 
+  //Ingestor do not accept ADE files
   @Test
-  void shouldNotMatchRegexConflictAppAndName() {
-    String container = "rtd-transactions-32489876908u74bh781e2db57k098c5ad034341i8u7y";
-    String blob = "ADE.99910.TRNLOG.20220228.203107.001.csv.pgp";
-    String blobUri = "/blobServices/default/containers/" + container + "/blobs/" + blob;
+  void shouldNotMatchRegexWrogName(CapturedOutput output) {
+    String blobAde = "ADE.99910.TRNLOG.20220228.203107.001.csv.pgp";
+    String blobUri = "/blobServices/default/containers/" + containerRtd + "/blobs/" + blobAde;
     BlobApplicationAware myBlob = new BlobApplicationAware(blobUri);
     assertSame(Status.INIT, myBlob.getStatus());
+    assertThat(output.getOut(), containsString("Wrong name format:"));
   }
-
 
   //The test parameters reproduce the following scenarios: blobUriShouldFailWrongService,
   // blobUriShouldFailNoService, blobUriShouldFailShortABI, blobUriShouldFailLongABI,
@@ -71,8 +108,7 @@ class BlobApplicationAwareTest {
       "CSTAR.99910.TRNLOG.20220228..001.csv.pgp", "CSTAR.99910.TRNLOG.20220228.103107.1.csv.pgp",
       "CSTAR.99910.TRNLOG.20220228.103107..csv.pgp"})
   void blobUriShouldFailRegex(String blobName, CapturedOutput output) {
-    String container = "rtd-transactions-32489876908u74bh781e2db57k098c5ad034341i8u7y";
-    String blobUri = "/blobServices/default/containers/" + container + "/blobs/" + blobName;
+    String blobUri = "/blobServices/default/containers/" + containerRtd + "/blobs/" + blobName;
     BlobApplicationAware myBlob = new BlobApplicationAware(blobUri);
     assertSame(Status.INIT, myBlob.getStatus());
   }
