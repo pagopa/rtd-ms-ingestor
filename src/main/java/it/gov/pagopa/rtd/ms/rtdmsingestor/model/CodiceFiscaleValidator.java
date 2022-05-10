624f312991e283379b6d6e035b5740a1e8f10e83
TommasoLencioni <tommaso.lencioni@pagopa.it>
2022-05-10 17:49:29 +0200

    [RTD-69] add fiscal code constraint and annotation validator.

---
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmsingestor/model/CodiceFiscaleValidator.java
@@ -0,0 +1,62 @@
+package it.gov.pagopa.rtd.ms.rtdmsingestor.model;
+
+import static it.gov.pagopa.rtd.ms.rtdmsingestor.model.FiscalCode.Response.EMPTY;
+import static it.gov.pagopa.rtd.ms.rtdmsingestor.model.FiscalCode.Response.INVALID_CHARACTERS;
+import static it.gov.pagopa.rtd.ms.rtdmsingestor.model.FiscalCode.Response.INVALID_CHECKSUM;
+import static it.gov.pagopa.rtd.ms.rtdmsingestor.model.FiscalCode.Response.INVALID_LENGTH;
+
+import it.gov.pagopa.rtd.ms.rtdmsingestor.model.FiscalCode.Response;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import org.hibernate.validator.constraintvalidation.HibernateConstraintValidatorContext;
+
+/**
+ * This is the implementation of a custom validator. This validator checks the correctness of an
+ * italian fiscal code.
+ */
+public class CodiceFiscaleValidator implements
+    ConstraintValidator<CodiceFiscaleConstraint, String> {
+
+  @Override
+  public boolean isValid(String codiceFiscale,
+      ConstraintValidatorContext context) {
+
+    //If codiceFiscale is null, then the validation is passed because it is not a mandatory field
+    if (codiceFiscale == null || codiceFiscale.equals("")) {
+      return true;
+    }
+
+    HibernateConstraintValidatorContext hibernateContext =
+        context.unwrap(HibernateConstraintValidatorContext.class);
+    hibernateContext.disableDefaultConstraintViolation();
+
+    Response checkedCodFis = FiscalCode.validate(codiceFiscale);
+
+    if (checkedCodFis.equals(INVALID_CHARACTERS)) {
+      hibernateContext.buildConstraintViolationWithTemplate(
+              "Invalid character for Fiscal Code " + codiceFiscale)
+          .addConstraintViolation();
+      return false;
+    }
+    if (checkedCodFis.equals(INVALID_LENGTH)) {
+      hibernateContext.buildConstraintViolationWithTemplate(
+              "Invalid length for Fiscal Code " + codiceFiscale)
+          .addConstraintViolation();
+      return false;
+    }
+    if (checkedCodFis.equals(INVALID_CHECKSUM)) {
+      hibernateContext.buildConstraintViolationWithTemplate(
+              "Invalid checksum for Fiscal Code " + codiceFiscale)
+          .addConstraintViolation();
+      return false;
+    }
+    if (checkedCodFis.equals(EMPTY)) {
+      hibernateContext.buildConstraintViolationWithTemplate(
+              "Empty Fiscal Code " + codiceFiscale)
+          .addConstraintViolation();
+      return false;
+    }
+    return true;
+  }
+
+}
