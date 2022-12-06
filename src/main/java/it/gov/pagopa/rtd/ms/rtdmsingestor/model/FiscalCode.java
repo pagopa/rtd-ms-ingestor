d3e482f90a846e76697359d788d153d5160ec335
Luca Consalvi <luca.consalvi@pagopa.it>
2022-12-06 11:08:26 +0100

    [RTD-984] fix major code smells

--- src/main/java/it/gov/pagopa/rtd/ms/rtdmsingestor/model/FiscalCode.java
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmsingestor/model/FiscalCode.java
@@ -59,7 +59,7 @@ public class FiscalCode {
     boolean[] homocode = new boolean[3];
 
     if (!cf.matches(
-        "^[A-Z]{6}\\d{2}[ABCDEHLMPRST]{1}\\d{2}[A-Z]{1}[0-9LMNPQRSTUV]{3}\\d{1}$")) {
+        "^[A-Z]{6}\\d{2}[ABCDEHLMPRST]\\d{2}[A-Z][0-9LMNPQRSTUV]{3}\\d$")) {
       return Response.INVALID_CHARACTERS;
     }
     int s = 0;
