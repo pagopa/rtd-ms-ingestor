298e3aeba38be4afa7597d38dd7bea0a782cf8a2
petretiandrea <petretiandrea@gmail.com>
2023-08-01 13:26:53 +0200

    added feature flag to disable anonymization + added new rtx trx

--- src/main/java/it/gov/pagopa/rtd/ms/rtdmsingestor/configuration/RepositoryConfiguration.java
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmsingestor/configuration/RepositoryConfiguration.java
@@ -1,8 +1,11 @@
 package it.gov.pagopa.rtd.ms.rtdmsingestor.configuration;
 
+import it.gov.pagopa.rtd.ms.rtdmsingestor.infrastructure.mongo.EPIItem;
 import it.gov.pagopa.rtd.ms.rtdmsingestor.infrastructure.repositories.IngestorDAO;
 import it.gov.pagopa.rtd.ms.rtdmsingestor.infrastructure.repositories.IngestorRepositoryImpl;
 import it.gov.pagopa.rtd.ms.rtdmsingestor.repository.IngestorRepository;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
@@ -12,8 +15,17 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
     basePackages = "it.gov.pagopa.rtd.ms.rtdmsingestor.infrastructure.repositories")
 public class RepositoryConfiguration {
 
+  @Value("#{new Boolean(${ingestor.anonymizePaymentInstrument})}")
+  private Boolean anonymizePaymentInstrument;
+
   @Bean
-  public IngestorRepository ingestorRepository(IngestorDAO ingestorDAO) {
-    return new IngestorRepositoryImpl(ingestorDAO);
+  public IngestorRepository ingestorRepository(
+      IngestorDAO ingestorDAO
+  ) {
+    if (anonymizePaymentInstrument) {
+      return new IngestorRepositoryImpl(ingestorDAO);
+    } else {
+      return hash -> Optional.of(EPIItem.builder().hashPan(hash).build());
+    }
   }
 }
