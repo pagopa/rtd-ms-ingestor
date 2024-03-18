2ff5cdd02b6c7437c477acd6b0c6706b1a2844ed
TommasoLencioni <tommaso.lencioni@pagopa.it>
2024-03-18 17:09:07 +0100

    feat: [CHK-2502] use lower camel case for wallet API payload

--- src/main/java/it/gov/pagopa/rtd/ms/rtdmsingestor/model/ContractMethodAttributes.java
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmsingestor/model/ContractMethodAttributes.java
@@ -1,6 +1,6 @@
 package it.gov.pagopa.rtd.ms.rtdmsingestor.model;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonAlias;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
@@ -14,31 +14,31 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class ContractMethodAttributes {
 
-  @JsonProperty("pan_tail")
+  @JsonAlias("pan_tail")
   @Pattern(regexp = "\\d{4}")
   private String panTail;
 
-  @JsonProperty("expdate")
+  @JsonAlias("expdate")
   @NotNull
   private String expdate;
 
-  @JsonProperty("card_id_4")
+  @JsonAlias("card_id_4")
   @NotNull
   private String cardId4;
 
-  @JsonProperty("card_payment_circuit")
+  @JsonAlias("card_payment_circuit")
   @NotNull
   private String cardPaymentCircuit;
 
-  @JsonProperty("new_contract_identifier")
+  @JsonAlias("new_contract_identifier")
   @NotNull
   private String newContractIdentifier;
 
-  @JsonProperty("original_contract_identifier")
+  @JsonAlias("original_contract_identifier")
   @NotNull
   private String originalContractIdentifier;
 
-  @JsonProperty("card_bin")
+  @JsonAlias("card_bin")
   @NotNull
   @Pattern(regexp = "\\d{6}")
   private String cardBin;
--- src/main/java/it/gov/pagopa/rtd/ms/rtdmsingestor/model/WalletContract.java
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmsingestor/model/WalletContract.java
@@ -1,8 +1,10 @@
 package it.gov.pagopa.rtd.ms.rtdmsingestor.model;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
@@ -15,31 +17,32 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonNaming(PropertyNamingStrategies.LowerCamelCaseStrategy.class)
 public class WalletContract {
 
-  @JsonProperty("action")
+  @JsonAlias("action")
   @Pattern(regexp = "CREATE|DELETE", flags = Pattern.Flag.CASE_INSENSITIVE)
   @NotNull
   private String action;
 
-  @JsonProperty("import_outcome")
+  @JsonAlias("import_outcome")
   @NotNull
   @Pattern(regexp = "OK|KO", flags = Pattern.Flag.CASE_INSENSITIVE)
   private String importOutcome;
 
-  @JsonProperty("payment_method")
+  @JsonAlias("payment_method")
   @JsonInclude(Include.NON_NULL)
   private String paymentMethod;
 
-  @JsonProperty("method_attributes")
+  @JsonAlias("method_attributes")
   @JsonInclude(Include.NON_NULL)
   private ContractMethodAttributes methodAttributes;
 
-  @JsonProperty("reason_message")
+  @JsonAlias("reason_message")
   @JsonInclude(Include.NON_NULL)
   private String reasonMessage;
 
-  @JsonProperty("original_contract_identifier")
+  @JsonAlias("original_contract_identifier")
   @JsonInclude(Include.NON_NULL)
   private String originalContractIdentifier;
 }
