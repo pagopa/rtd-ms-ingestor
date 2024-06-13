fedab1f161b5086e32d5748ddb5e13597580882e
Luca Consalvi <luca.consalvi@pagopa.it>
2024-06-13 16:27:48 +0200

    [RTD-2380] change binder options

--- src/main/java/it/gov/pagopa/rtd/ms/rtdmsingestor/telemetry/SpringCloudKafkaBinderInstrumentation.java
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmsingestor/telemetry/SpringCloudKafkaBinderInstrumentation.java
@@ -13,30 +13,31 @@ import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.listener.AbstractMessageListenerContainer;
 
 /**
- * The `SpringCloudKafkaBinderInstrumentation` class is responsible for
- * configuring and customizing
+ * The `SpringCloudKafkaBinderInstrumentation` class is responsible for configuring and customizing
  * Kafka-related components to use opentelemetry tracing.
  * <p>
- * The `binderCustomizer` method configures the Kafka binder to be traced with
- * opentelemetry
- * by setting a custom client factory and by customizing the kafka container.
- * The `customizedKafkaClient` wrap the producer using opentelemetry
- * instrumentation kafka library,
- * while `containerCustomizer` customize the container (consumer) using a spring
- * kafka opentelemetry library
+ * The `binderCustomizer` method configures the Kafka binder to be traced with opentelemetry by
+ * setting a custom client factory and by customizing the kafka container. The
+ * `customizedKafkaClient` wrap the producer using opentelemetry instrumentation kafka library,
+ * while `containerCustomizer` customize the container (consumer) using a spring kafka opentelemetry
+ * library
  * <p>
  * Overall, this class allows to observe and trace all Kafka interactions.
  */
 public class SpringCloudKafkaBinderInstrumentation {
 
   @Bean
-  public BinderCustomizer binderCustomizer(ApplicationContext context) {
+  public BinderCustomizer binderCustomizer(
+      ApplicationContext context
+  ) {
     return (binder, binderName) -> {
       if ((Object) binder instanceof KafkaMessageChannelBinder kafkaMessageChannelBinder) {
         kafkaMessageChannelBinder.setContainerCustomizer(
-            context.getBean("containerCustomizer", ListenerContainerCustomizer.class));
+            context.getBean("containerCustomizer", ListenerContainerCustomizer.class)
+        );
         kafkaMessageChannelBinder.addClientFactoryCustomizer(context.getBean(
-            "customizedKafkaClient", ClientFactoryCustomizer.class));
+            "customizedKafkaClient", ClientFactoryCustomizer.class
+        ));
       }
     };
   }
@@ -58,7 +59,8 @@ public class SpringCloudKafkaBinderInstrumentation {
 
   @Bean
   public ListenerContainerCustomizer<AbstractMessageListenerContainer<?, ?>> containerCustomizer(
-      OpenTelemetry openTelemetry) {
+      OpenTelemetry openTelemetry
+  ) {
     final var springKafkaTelemetry = SpringKafkaTelemetry.builder(openTelemetry)
         .setMessagingReceiveInstrumentationEnabled(true)
         .setCaptureExperimentalSpanAttributes(true)
