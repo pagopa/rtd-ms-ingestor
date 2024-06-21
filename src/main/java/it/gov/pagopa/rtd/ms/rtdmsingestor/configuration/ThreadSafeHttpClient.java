ed50d0e171638ae796c1487787fd1f9af0eb1579
TommasoLencioni <tommaso.lencioni@pagopa.it>
2024-06-21 12:40:24 +0200

    chore: add logging to HTTP Client

--- src/main/java/it/gov/pagopa/rtd/ms/rtdmsingestor/configuration/ThreadSafeHttpClient.java
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmsingestor/configuration/ThreadSafeHttpClient.java
@@ -4,6 +4,9 @@ import java.security.KeyManagementException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import javax.net.ssl.SSLContext;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpRequestInterceptor;
+import org.apache.http.HttpResponseInterceptor;
 import org.apache.http.config.Registry;
 import org.apache.http.config.RegistryBuilder;
 import org.apache.http.conn.socket.ConnectionSocketFactory;
@@ -12,6 +15,7 @@ import org.apache.http.conn.ssl.NoopHostnameVerifier;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.DefaultConnectionKeepAliveStrategy;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.apache.http.ssl.SSLContexts;
@@ -22,6 +26,7 @@ import org.springframework.context.annotation.Configuration;
  * Thread safe HTTP client implementing pooling.
  */
 @Configuration
+@Slf4j
 public class ThreadSafeHttpClient {
 
   @Bean
@@ -41,6 +46,20 @@ public class ThreadSafeHttpClient {
     connectionManager.setMaxTotal(25);
     connectionManager.setDefaultMaxPerRoute(25);
 
-    return HttpClients.custom().setConnectionManager(connectionManager).build();
+    return HttpClients.custom()
+        .setConnectionManager(connectionManager)
+        .setKeepAliveStrategy(new DefaultConnectionKeepAliveStrategy())
+        .addInterceptorFirst((HttpRequestInterceptor) (request, context) -> {
+          log.info(
+              "Before - Leased Connections = " + connectionManager.getTotalStats().getLeased());
+          log.info("Before - Available Connections = " + connectionManager.getTotalStats()
+              .getAvailable());
+        })
+        .addInterceptorFirst((HttpResponseInterceptor) (response, context) -> {
+          log.info("After - Leased Connections = " + connectionManager.getTotalStats().getLeased());
+          log.info("After - Available Connections = " + connectionManager.getTotalStats()
+              .getAvailable());
+        })
+        .build();
   }
 }
